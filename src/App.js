efc683c281757203196db4a15a91e4111aabfe32
Ahmed Atta <ahmedelsaikaly@gmail.com>
2020-07-11 11:22:09 +0300

    Header component finshed with half css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header.js"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello from react</h1>
     </div>
   );
