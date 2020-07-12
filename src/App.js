8bbfa49ef052e75342031d507880278bec195919
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-07-12 13:24:13 +0300

    Header Component integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Search from './Components/Search By Breed Component/SearchByBreed'
+import Header from "./Components/HeaderComponent/Header.js"
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <hr/>
       <Search />
     </div>
   );
